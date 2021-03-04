34749f04e069462ac7ba8a826d6630360a608c92
andreassef <andreassef02@gmail.com>
2021-03-04 13:27:45 -0400

    [UPDATE] - Adicao de SVG e nova funcao para deletar card

--- src/components/CardNotes/CardNotes.jsx
+++ src/components/CardNotes/CardNotes.jsx
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
-import "./styles.css"
+import "./styles.css";
+import {ReactComponent as DeleteSVG} from '../../assets/images/delete.svg'
 
 class CardNotes extends Component {
     constructor(props){
         super();
     }
 
+    delete() {
+        const indice = this.props.index
+        this.props.deleteNote(indice)
+
+    }
+
     render() {
         return (
             <section className="card-nota">
-                <head className="card-nota_cabecalho"><h3>{this.props.title}</h3></head>
+                <head className="card-nota_cabecalho">
+                    <h3>{this.props.title}</h3>
+                    <DeleteSVG onClick={this.delete.bind(this)} />
+                </head>
                 <p className="card-nota_texto">{this.props.text}</p>
             </section> 
         );
