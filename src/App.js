1a046aa9f622b52600ed6bba3c83af747e71cf3b
andreassef <andreassef02@gmail.com>
2021-03-03 22:20:26 -0400

    [UPATDE] - Refatoracao para class component e adicao de logica para salvar card

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import Notes from "./components/notes";
-import RegisterForm from "./components/registerForm";
+import Notes from "./components/Notes/notes";
+import RegisterForm from "./components/RegisterForm/registerForm";
+import "./assets/App.css";
+import './assets/index.css';
+import React, { Component} from "react";
 
-function App() {
-  return (
-    <section>
-      <RegisterForm />
-      <Notes />
-    </section>
-  );
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      notes: []
+    }
+  }
+
+  createCard(title, text){
+    const newNote = {title, text};
+    const newArrayNotes = [...this.state.notes, newNote];
+    const newState = {
+      notes: newArrayNotes
+    }
+    this.setState(newState)
+  }
+
+  render() {
+    return (
+      <section className="conteudo">
+        <RegisterForm createCard = {this.createCard.bind(this)} />
+        <Notes notes={this.state.notes} />
+      </section>
+    );
+  }
 }
 
 export default App;
