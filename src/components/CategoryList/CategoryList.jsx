99d03b97c8b30176e8fae33b2ea2361eb5ddbcd8
andreassef <andreassef02@gmail.com>
2021-03-05 23:54:27 -0400

    [NEW] - Criacao de novo component, categoria de lista

---
+++ src/components/CategoryList/CategoryList.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class CategoryList extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <ul>
+                <li>Categorias</li>
+                <li>Categorias</li>
+                <li>Categorias</li>
+            </ul>
+         );
+    }
+}
+ 
+export default CategoryList;
