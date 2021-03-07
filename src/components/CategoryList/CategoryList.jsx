61213c4501dd23a90b3614f23886d802f3949f80
Andre Assef <andreassef02@gmail.com>
2021-03-06 23:36:56 -0400

    [UPDATE] - adicao de logica para renderizacao de novas categorias adicionadas dinamicamente

--- src/components/CategoryList/CategoryList.jsx
+++ src/components/CategoryList/CategoryList.jsx
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
+import "./styles.css"
 
 class CategoryList extends Component {
-    state = {  }
+    
+    handleEventInput(e) {
+        if (e.key == "Enter") {
+            let valorCategoria = e.target.value;
+            this.props.adicionarCategoria(valorCategoria);
+        }
+    }
+
     render() { 
         return ( 
-            <ul>
-                <li>Categorias</li>
-                <li>Categorias</li>
-                <li>Categorias</li>
-            </ul>
+            <section className="lista-categorias">
+                <ul className="lista-categorias_lista">
+                    {this.props.categorias.map((categoria, index) => {
+                        return <li className="lista-categorias_item">{categoria}</li>
+                    })}
+                </ul>
+                <input type="text"
+                    className="lista-categorias_input"
+                    placeholder="type something"
+                    onKeyUp ={this.handleEventInput.bind(this)}
+                />
+            </section>
          );
     }
 }
