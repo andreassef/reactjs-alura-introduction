962288857220c0eb7557fb0761c7c3f8acd78246
andreassef <andreassef02@gmail.com>
2021-04-07 23:58:37 -0400

    [UPDATE] - Refatoracao de codigo do App

--- src/App.js
+++ src/App.js
@@ -4,48 +4,26 @@ import "./assets/App.css";
 import './assets/index.css';
 import React, { Component} from "react";
 import CategoryList from "./components/CategoryList/CategoryList";
+import Categorias from "./dados/Categorias";
+import ArrayDeNotas from "./dados/Notas";
 
 class App extends Component {
 
   constructor() {
     super();
-    this.state = {
-      notes: [],
-      categorias: []
-    }
-  }
-
-  createCard(title, text, categoria){
-    const newNote = {title, text, categoria};
-    const newArrayNotes = [...this.state.notes, newNote];
-    const newState = {
-      notes: newArrayNotes
-    }
-    this.setState(newState)
-  }
-
-  adicionarCategoria(nomeCategoria) {
-    const novoArrayCategorias = [...this.state.categorias, nomeCategoria];
-    //const novoEstado = {...this.state, categorias: novoArrayCategorias};
-    this.setState({categorias: novoArrayCategorias});
-  }
-
-  deleteNote(index) {
-    let arrayOfNotes = this.state.notes
-    arrayOfNotes.splice(index, 1);
-    this.setState({notes: arrayOfNotes})
-    console.log('Note was delete')
+    this.categorias = new Categorias();
+    this.notas = new ArrayDeNotas();
   }
 
   render() {
     return (
       <section className="conteudo">
-        <RegisterForm categorias={this.state.categorias} createCard = {this.createCard.bind(this)} />
+        <RegisterForm categorias={this.categorias} createCard = {this.notas.adicionarNota.bind(this.notas)} />
         <main className="main-content"> 
           <CategoryList 
-            adicionarCategoria={this.adicionarCategoria.bind(this)}
-            categorias={this.state.categorias} />
-          <Notes deleteNote={this.deleteNote.bind(this)} notes={this.state.notes} />
+            adicionarCategoria={this.categorias.adicionarCategoria.bind(this.categorias)}
+            categorias={this.categorias} />
+          <Notes deleteNote={this.notas.apagarNota} notes={this.notas} />
         </main>
       </section>
     );
